141b561c6258ce4c0766c02eb40f3b1fa0713796
Ivan Borovyk <saref012@gmail.com>
2026-03-04 21:32:26 +0200

    render tasks as tickets

--- src/app/column.rs
+++ src/app/column.rs
@@ -1,9 +1,12 @@
+use std::fmt::Debug;
+
+use chrono::{Datelike, SubsecRound, TimeZone, Utc};
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, HorizontalAlignment, Layout, Rect},
     style::Stylize,
     text::Line,
-    widgets::{Block, List, Widget},
+    widgets::{Block, List, ListItem, Paragraph, Widget},
 };
 
 use crate::app::Task;
@@ -25,9 +28,47 @@ impl Widget for Column {
         Self: Sized,
     {
         let title = Line::from(self.name.as_str().bold());
-        let block = Block::bordered().title(title.centered());
-        let list = List::new(self.items.iter().map(|t| t.title.to_string())).block(block);
+        let block = if self.items.len() == 0 {
+            Block::bordered().title(title.centered())
+        } else {
+            Block::new().title(title.centered())
+        };
 
-        list.render(area, buf);
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(
+                self.items
+                    .iter()
+                    .map(|_| Constraint::Length(3))
+                    .collect::<Vec<_>>(),
+            )
+            .split(block.inner(area));
+
+        for (chunk, t) in chunks.iter().zip(&self.items) {
+            let last_update = Utc.timestamp_micros(t.updated_at).unwrap();
+            let last_update = format!(
+                "{}, {}, {}, {}",
+                last_update.year(),
+                if last_update.month() > 9 {
+                    last_update.month().to_string()
+                } else {
+                    "0".to_owned() + last_update.month().to_string().as_str()
+                },
+                if last_update.day() > 9 {
+                    last_update.day().to_string()
+                } else {
+                    "0".to_owned() + last_update.day().to_string().as_str()
+                },
+                last_update.time().round_subsecs(0)
+            );
+
+            let p = Paragraph::new(t.title.as_str()).block(
+                Block::bordered()
+                    .title_bottom(last_update)
+                    .title_alignment(HorizontalAlignment::Right),
+            );
+            p.render(*chunk, buf);
+        }
+        block.render(area, buf);
     }
 }
--- src/db.rs
+++ src/db.rs
@@ -26,7 +26,7 @@ pub fn create_bd() -> Result<Connection, AppError> {
 }
 
 pub fn create_task(conn: &Connection, title: String) -> Result<(), AppError> {
-    let now = Utc::now().timestamp().to_string();
+    let now = Utc::now().timestamp_micros().to_string();
     conn.execute(
         "INSERT INTO tasks (title, status, created_at, updated_at) VALUES (?1, ?2, ?3, ?4)",
         [title, Status::ToDo.into(), now.to_owned(), now.to_owned()],
